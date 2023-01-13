abad90366c1616d15ac7c5b69feb3790f8bb5bf4
Ozzie Harb <ozharb@gmail.com>
2023-01-13 10:21:38 -0800

    updated api endpoint to render

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: 'https://peaceful-shelf-32168.herokuapp.com/api',
-    //TestingEndpoint://
-    //API_ENDPOINT: 'http://localhost:8000/api',
-    TOKEN_KEY: 'budgitz-client-auth-token',
-    
-  }
+  API_ENDPOINT: "https://budgitz-api-render.onrender.com/api",
+  //TestingEndpoint://
+  //API_ENDPOINT: 'http://localhost:8000/api',
+  TOKEN_KEY: "budgitz-client-auth-token",
+};
